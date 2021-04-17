8ad985b1f661c4536e1ab8c3b57e322fc74ecf55
aramosvizcarra <aramosvizcarra@gmail.com>
2021-04-17 09:26:08 -0400

    Updated project with releases from 2021

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using API.Data;
+using API.Entities;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+
+namespace API.Controllers
+{    
+    public class UsersController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+
+        }
+        [HttpGet("{id}")]
+        [Authorize]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            return user;
+        }
+    }
+}
